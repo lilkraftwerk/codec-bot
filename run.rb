036feef0b27e1a233bf2d667e9437512b4c2d9ed
lilkraftwerk <lucasquinnmathis@gmail.com>
2015-09-06 10:51:14 +0200

    puts more things to console

--- run.rb
+++ run.rb
@@ -4,8 +4,8 @@ require_relative 'custom_twitter'
 def run
   snake = MGSTwitter.new
   snake.do_it
-  snake.results.each do |result|
-    puts "still running..."
+  snake.results.each_with_index do |result, index|
+    puts "item #{index + 1} of #{snake.results.length}"
     codec = CodecCreator.new(result)
     codec.do_it
   end
